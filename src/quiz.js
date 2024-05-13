b42f64b2e2fd5b3813c9193ff04ee3feeb0bd79a
Farnoush Daliran <farnoushfdf@gmail.com>
2024-05-13 20:46:27 +0200

    JavaScript Quiz Day 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,46 @@
-class Quiz {
+    // class Quiz 
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+class Quiz {
+        constructor(questions, timeLimit, timeRemaining) {
+          this.questions = questions;
+          this.timeLimit = timeLimit;
+          this.timeRemaining = timeRemaining;
+          this.correctAnswers = 0;
+          this.currentQuestionIndex = 0;
+        }
+      
+      
 
     // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
     // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
 
     // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        const currentQuestion = this.getQuestion();
+        if (currentQuestion.answer === answer) {
+            this.correctAnswers++;
+        }
+    }
 
     // 6. hasEnded()
+    hasEnded() {
+        return this.currentQuestionIndex === this.questions.length;
+    }
 }
